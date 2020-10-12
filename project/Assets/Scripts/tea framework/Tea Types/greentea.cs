c9367d63c445ca3ca40eb86e85db54bc25e49d97
unknown <sykesduncan32@gmail.com>
2020-10-12 22:54:06 +1100

    Feature : 1119 : Created basis for tea framework

---
+++ project/Assets/Scripts/tea framework/Tea Types/greentea.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class greentea : ingredient
+{
+    greentea()
+    {
+        name = "Green Tea";
+        scoreNumber = 10;
+    }
+    
+    
+}
---
+++ project/Assets/Scripts/tea framework/ingredient.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class ingredient : MonoBehaviour
+{
+
+    protected string name;
+    protected int scoreNumber;
+
+    public void SetName(string _name)
+    {
+        name = _name;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetScoreNumber(int score)
+    {
+        scoreNumber = score;
+    }
+
+    public int GetScoreNumber()
+    {
+        return scoreNumber;
+    }
+
+    
+
+}
