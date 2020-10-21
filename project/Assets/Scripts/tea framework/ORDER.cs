8900c296ecb1b75a58c928dfed13515be736a502
Duncan Sykes <s200490@students.aie.edu.au>
2020-10-21 15:05:46 +1100

    lol

--- project/Assets/Scripts/tea framework/ORDER.cs
+++ project/Assets/Scripts/tea framework/ORDER.cs
@@ -36,28 +36,17 @@ public class ORDER : MonoBehaviour
 
     private void Start()
     {
-        
-        foreach(var PleaseLetItEnd in Orders)
+        foreach (var PleaseLetItEnd in Orders)
         {
             Orders order = new Orders(PleaseLetItEnd.title, PleaseLetItEnd.ingredients);
-
             orderList.Add(order);
-
         }
 
-        foreach(var gay in orderList)
+        foreach (var ord in orderList)
         {
-            Debug.Log("_________________");
-            Debug.Log(" | " + gay.name + " | " + gay.score);
-            foreach (var lol in gay.ingredients)
-            {
-                Debug.Log(" | " + lol.name + " | ");
-            }
-            Debug.Log("_________________");
-
+            Debug.Log(ord.name + " | " + ord.score);
+            
         }
-        
-        
     }
 
     private void Update()
