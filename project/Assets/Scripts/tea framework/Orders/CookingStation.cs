a6b2960ed1548c0a353d9a8266a8eb73fdc88810
Duncan Sykes <s200490@students.aie.edu.au>
2020-10-23 10:28:34 +1100

    Feature (Codebase) : 1134 : Added functionality to cooking station. Checks for correct ingredients based on order and increases score based on orderscore

---
+++ project/Assets/Scripts/tea framework/Orders/CookingStation.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingStation : MonoBehaviour
+{
+    
+    private List<ingredient> playerInventory;
+    private List<Orders> ordersList;
+
+    private List<Orders> cookingList;
+    private float totalScore = 0;
+
+    public void Init(List<ingredient> player)
+    {
+
+    }
+
+    public void CheckOrder()
+    {
+
+    }
+
+    public void loadOrder(List<Orders> orders)
+    {
+        ordersList = orders;
+    }
+
+
+    public void AcceptIngredient(ingredient ingredient)
+    {
+        foreach(var check in ordersList)
+        {
+            foreach (var ingred in check.ingredients)
+            {
+                if (ingredient == ingred)
+                {
+                    totalScore += check.score;
+                    Debug.Log("Correct");
+                }
+                
+            }
+        }
+
+
+        Debug.Log(totalScore);
+
+    }
+
+
+
+    
+}
