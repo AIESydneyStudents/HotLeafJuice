477328da934c9794786dacab70d22827c9429e23
jerky14 <64898350+jerky14@users.noreply.github.com>
2020-11-26 12:11:46 +1100

    footsteps script

---
+++ project/Assets/footsteps.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footsteps : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+
+    private AudioSource AudioSource;
+
+    private void Awake()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    private void Step()
+    {
+        AudioClip clip = GetRandomClip();
+        AudioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[Random.RandomRange(0, clips.Length)];
+    }
+
+
+
+}
